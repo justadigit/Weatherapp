a30aab5446c1a4dbd8e6b7283fce9af30d2d9e54
NayWin23 <letmecode123456@gmail.com>
2020-03-01 23:04:14 +0630

    weather application

---
+++ forecast.js
@@ -0,0 +1,27 @@
+class Forecast{
+    constructor(){
+        this.key =  "n1QrFrDSlkTpx9EHA9DIlUIAtXWGRSxo";
+        this.weatherURI = "http://dataservice.accuweather.com/currentconditions/v1/";
+        this.cityURI = "http://dataservice.accuweather.com/locations/v1/cities/search";
+    };
+    async updateCity(city){
+        let CityData = await this.getCity(city);
+        let WeatherData = await this.getWeather(CityData.Key);
+        return {
+            CityData:CityData,
+            WeatherData:WeatherData
+            };
+    };
+    async getWeather(id){
+        const query = `${id}?apikey=${this.key}`;
+        const response = await fetch(this.weatherURI+query);
+        const data = await response.json();
+        return data[0];
+    };
+    async getCity(city){
+        const query = `?apikey=${this.key}&q=${city}`;
+        const response = await fetch(this.cityURI+query);
+        const data = await response.json();
+        return data[0];
+    };
+}
